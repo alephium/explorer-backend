ef967668aecd67ceb3bc873e995167d681493da1
Thomas Droxler <thomas.droxler@gmail.com>
2020-05-19 14:33:19 +0200

    Use timestamp codec from `common`

--- src/main/scala/org/alephium/explorer/api/model/BlockEntry.scala
+++ src/main/scala/org/alephium/explorer/api/model/BlockEntry.scala
@@ -3,8 +3,7 @@ package org.alephium.explorer.api.model
 import io.circe.Codec
 import io.circe.generic.semiauto.deriveCodec
 
-import org.alephium.explorer.api.Circe.timestampCodec
-import org.alephium.rpc.CirceUtils.avectorCodec
+import org.alephium.rpc.CirceUtils.{avectorCodec, timestampCodec}
 import org.alephium.util.{AVector, TimeStamp}
 
 final case class BlockEntry(
