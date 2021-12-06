8699123b028e0b4d4983ca3536f545302fe8d8ae
Thomas Droxler <thomas.droxler@gmail.com>
2021-12-06 17:00:48 +0100

    Rename `script_hint` column to `hint`

--- app/src/main/scala/org/alephium/explorer/persistence/schema/InputSchema.scala
+++ app/src/main/scala/org/alephium/explorer/persistence/schema/InputSchema.scala
@@ -31,11 +31,10 @@ trait InputSchema extends CustomTypes {
   import config.profile.api._
 
   class Inputs(tag: Tag) extends Table[InputEntity](tag, "inputs") {
-    def blockHash: Rep[BlockEntry.Hash] = column[BlockEntry.Hash]("block_hash")
-    def txHash: Rep[Transaction.Hash]   = column[Transaction.Hash]("tx_hash")
-    def timestamp: Rep[TimeStamp]       = column[TimeStamp]("timestamp")
-    //TODO Rename column field when we release a version that have to dump data.
-    def hint: Rep[Int]                    = column[Int]("script_hint")
+    def blockHash: Rep[BlockEntry.Hash]   = column[BlockEntry.Hash]("block_hash")
+    def txHash: Rep[Transaction.Hash]     = column[Transaction.Hash]("tx_hash")
+    def timestamp: Rep[TimeStamp]         = column[TimeStamp]("timestamp")
+    def hint: Rep[Int]                    = column[Int]("hint")
     def outputRefKey: Rep[Hash]           = column[Hash]("output_ref_key")
     def unlockScript: Rep[Option[String]] = column[Option[String]]("unlock_script")
     def mainChain: Rep[Boolean]           = column[Boolean]("main_chain")
