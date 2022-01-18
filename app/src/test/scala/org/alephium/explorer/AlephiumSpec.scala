1584282b5d8016673424a7a8b5b9662304ec5a11
simerplaha <simer.j@gmail.com>
2022-01-18 19:42:35 +1100

    #101 - Added `isnot` to `AlephiumSpec`

--- app/src/test/scala/org/alephium/explorer/AlephiumSpec.scala
+++ app/src/test/scala/org/alephium/explorer/AlephiumSpec.scala
@@ -33,8 +33,10 @@ trait AlephiumSpec extends AnyFlatSpecLike with ScalaCheckDrivenPropertyChecks w
   // scalastyle:off no.should
   implicit class IsOps[A: Equality](left: A)(implicit pos: Position) {
     // scalastyle:off scalatest-matcher
-    def is(right: A): Assertion                          = left shouldEqual right
-    def is(right: ResultOfATypeInvocation[_]): Assertion = left shouldBe right
+    def is(right: A): Assertion                             = left shouldEqual right
+    def is(right: ResultOfATypeInvocation[_]): Assertion    = left shouldBe right
+    def isnot(right: A): Assertion                          = left should not equal right
+    def isnot(right: ResultOfATypeInvocation[_]): Assertion = left should not be right
     // scalastyle:on scalatest-matcher
   }
 }
