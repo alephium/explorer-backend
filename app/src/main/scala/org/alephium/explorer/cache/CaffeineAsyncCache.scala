5fda25ef27122e647843c7a3c8283de8b7142349
simerplaha <simer.j@gmail.com>
2022-03-28 16:53:34 +1100

    #119: Added `CaffeineAsyncCache`

---
+++ app/src/main/scala/org/alephium/explorer/cache/CaffeineAsyncCache.scala
@@ -0,0 +1,64 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.explorer.cache
+
+import java.util.concurrent.CompletableFuture
+
+import scala.concurrent.Future
+import scala.jdk.CollectionConverters._
+import scala.jdk.FutureConverters._
+
+import com.github.benmanes.caffeine.cache.AsyncLoadingCache
+
+object CaffeineAsyncCache {
+
+  @inline def apply[K, V](cache: AsyncLoadingCache[K, V]): CaffeineAsyncCache[K, V] =
+    new CaffeineAsyncCache[K, V](cache)
+
+}
+
+/** A wrapper around [[AsyncLoadingCache]] to convert Java types to Scala. */
+class CaffeineAsyncCache[K, V](cache: AsyncLoadingCache[K, V]) {
+
+  def get(key: K): Future[V] =
+    cache.get(key).asScala
+
+  def getIfPresent(key: K): Option[Future[V]] = {
+    val valueOrNull = cache.getIfPresent(key)
+    if (valueOrNull == null) {
+      None
+    } else {
+      Some(valueOrNull.asScala)
+    }
+  }
+
+  def put(key: K, value: V): Unit =
+    cache.put(key, CompletableFuture.completedFuture(value))
+
+  def put(key: K, value: Future[V]): Unit =
+    cache.put(key, value.asJava.toCompletableFuture)
+
+  def invalidate(key: K): Unit =
+    cache.synchronous().invalidate(key)
+
+  def invalidateAll(key: Iterable[K]): Unit =
+    cache.synchronous().invalidateAll(key.asJava)
+
+  def invalidateAll(): Unit =
+    cache.synchronous().invalidateAll()
+
+}
