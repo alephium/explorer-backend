56e87fbaaf5564016f7632ff8b20e07050931a6a
simerplaha <simer.j@gmail.com>
2022-04-21 19:16:46 +1000

    #200: Added `AsyncReloadingCache`

---
+++ app/src/main/scala/org/alephium/explorer/cache/AsyncReloadingCache.scala
@@ -0,0 +1,115 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.explorer.cache
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.{Deadline, FiniteDuration}
+import scala.util.{Failure, Success}
+
+import com.typesafe.scalalogging.LazyLogging
+
+object AsyncReloadingCache {
+
+  /**
+    * Example:
+    * {{{
+    *    val cache =
+    *      AsyncReloadingCache(initial = "Initial value", reloadAfter = 1.second) {
+    *        currentValue: String =>
+    *          Future {
+    *            currentValue + " updated"
+    *          }
+    *       }
+    *
+    *     val value: String = cache.get()
+    * }}}
+    */
+  @inline def apply[T](initial: T, reloadAfter: FiniteDuration)(
+      loader: T => Future[T]): AsyncReloadingCache[T] =
+    new AsyncReloadingCache[T](
+      value       = initial,
+      deadline    = reloadAfter.fromNow,
+      reloading   = new AtomicBoolean(),
+      reloadAfter = reloadAfter,
+      loader      = loader
+    )
+
+}
+
+/**
+  * Cache that reloads value asynchronously returning the existing
+  * cached value while reload/re-cache is in the progress in the background.
+  *
+  * @param value       Current cached value
+  * @param deadline    Deadline until expiration
+  * @param reloading   Allows only a single thread to execute reloading
+  * @param reloadAfter How often to reload the cache or how long to keep existing cache value alive.
+  * @param loader      A function given the existing cached value returns the next value to cache.
+  * @tparam T Cached value's type
+  */
+class AsyncReloadingCache[T](@volatile private var value: T,
+                             @volatile private var deadline: Deadline,
+                             reloading: AtomicBoolean,
+                             reloadAfter: FiniteDuration,
+                             loader: T => Future[T])
+    extends LazyLogging {
+
+  /** Returns the latest cached value and invokes reload */
+  def get(): T = {
+    reload()
+    value
+  }
+
+  /**
+    * Instantly expires the current cached value and invokes reload.
+    *
+    * @note This does not always guarantee instant reload will occur immediately in this
+    *       call for cases when another thread is currently successfully executing reload.
+    * */
+  def expireAndReload(): Unit = {
+    expire()
+    reload()
+  }
+
+  /** Sets the current cached value as expired which gets reload on next [[get]] */
+  def expire(): Unit =
+    deadline = Deadline.now
+
+  private def reload(): Unit =
+    //Reload if expired and not already being reloaded by another thread
+    if (deadline.isOverdue() && reloading.compareAndSet(false, true)) {
+      //fetch and set next cache state while staying in the current
+      //ExecutionContext since the state update is inexpensive.
+      loader(value).onComplete { result =>
+        result match {
+          case Success(value) =>
+            this.value = value
+
+          case Failure(exception) =>
+            //Fatal error: Log for debugging.
+            logger.error("Failed to reload cache", exception)
+        }
+
+        //set next deadline
+        deadline = reloadAfter.fromNow
+        //release to allow next reload
+        reloading.set(false)
+      }(ExecutionContext.parasitic)
+    }
+}
---
+++ app/src/test/scala/org/alephium/explorer/cache/AsyncReloadingCacheSpec.scala
@@ -0,0 +1,117 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.explorer.cache
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
+import org.scalatest.concurrent.PatienceConfiguration.Timeout
+
+import org.alephium.explorer.AlephiumSpec
+
+class AsyncReloadingCacheSpec extends AlephiumSpec with ScalaFutures with Eventually {
+
+  implicit val executionContext: ExecutionContext = ExecutionContext.global
+
+  it should "reload cache after expiration while returning existing value even during reloading" in {
+    val reloadCount = new AtomicInteger() //number of times reload was executed
+
+    //a cache with initial value as 1 and reload set to 1.second
+    val cache =
+      AsyncReloadingCache(initial = 1, reloadAfter = 1.second) { currentValue =>
+        Future {
+          reloadCount.incrementAndGet() //reload invoked increment value
+          currentValue + 1
+        }
+      }
+
+    cache.get() is 1 //Returns initial cached value. There is time left to expiration
+    reloadCount.get() is 0 //No reload yet
+
+    eventually(Timeout(1.5.seconds))(cache.get() is 2) //eventually reload occurs with cache updated
+    reloadCount.get() is 1 //first reload
+
+    eventually(Timeout(1.5.seconds))(cache.get() is 3) //another reload & cache updated
+    reloadCount.get() is 2 //second reload
+
+    eventually(Timeout(1.5.seconds))(cache.get() is 4) //another reload & cache updated
+    reloadCount.get() is 3 //third reload
+  }
+
+  it should "not allow multiple threads to concurrently execute reload" in {
+    val reloadCount = new AtomicInteger() //number of times reload was executed
+
+    //a cache with initial value as 1 and reload set to 2.seconds
+    val cache =
+      AsyncReloadingCache(initial = 1, reloadAfter = 2.seconds) { currentValue =>
+        Future {
+          reloadCount.incrementAndGet()
+          currentValue + 1
+        }
+      }
+
+    //dispatch multiple threads to concurrently execute cache.get() which triggers reload
+    def concurrentlyReadCache(expectedCachedValue: Int) =
+      Future.sequence(List.fill(100)(Future(cache.get()))).futureValue is
+        List.fill(100)(expectedCachedValue)
+
+    concurrentlyReadCache(1)
+    reloadCount.get() is 0 //Initial value gets returned so not reload occur.
+
+    eventually(Timeout(3.seconds)) {
+      concurrentlyReadCache(2)
+      reloadCount.get() is 1
+    }
+  }
+
+  it should "expireAndReload immediately" in {
+    val reloadCount = new AtomicInteger() //number of times reload was executed
+
+    //long 1.hour reload timeout to test manual expireAndReload
+    val cache =
+      AsyncReloadingCache(initial = 1, reloadAfter = 1.hour) { currentValue =>
+        Future {
+          reloadCount.incrementAndGet()
+          currentValue + 1
+        }
+      }
+
+    //dispatch multiple threads to concurrently execute cache.get() which triggers reload
+    def concurrentlyReadCache(expectedCachedValue: Int) =
+      Future.sequence(List.fill(100)(Future(cache.get()))).futureValue is
+        List.fill(100)(expectedCachedValue)
+
+    cache.expireAndReload()
+    concurrentlyReadCache(2)
+    reloadCount.get() is 1 //Initial value gets returned so not reload occur.
+
+    cache.expireAndReload()
+    eventually(Timeout(1.second)) {
+      concurrentlyReadCache(3)
+      reloadCount.get() is 2
+    }
+
+    cache.expireAndReload()
+    eventually(Timeout(1.second)) {
+      concurrentlyReadCache(4)
+      reloadCount.get() is 3
+    }
+  }
+}
