28d2789f8135053613c4521ff88f406c34750d52
Thomas Droxler <thomas.droxler@gmail.com>
2022-06-13 17:30:22 +0200

    Add `Token` model

---
+++ app/src/main/scala/org/alephium/explorer/api/model/Token.scala
@@ -0,0 +1,34 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+
+package org.alephium.explorer.api.model
+
+import org.alephium.explorer.api.Json.{hashReadWriter, u256ReadWriter}
+import org.alephium.json.Json._
+import org.alephium.protocol.Hash
+import org.alephium.serde._
+import org.alephium.util.U256
+
+final case class Token(id: Hash, amount: U256)
+
+object Token {
+  implicit val readWriter: ReadWriter[Token] = macroRW
+  implicit val serde: Serde[Token] =
+    Serde.forProduct2(
+      Token.apply,
+      t => (t.id, t.amount)
+    )
+}
