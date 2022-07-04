f11dd2ada25023bb4ae6e7bc95971c91dc9c93c7
simerplaha <simer.j@gmail.com>
2022-07-04 16:12:48 +1000

    #206: Transactional `BlockDao.updateInputs`

---
+++ app/src/test/scala/org/alephium/explorer/GenModel.scala
@@ -0,0 +1,55 @@
+// Copyright 2018 The Alephium Authors
+// This file is part of the alephium project.
+//
+// The library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the library. If not, see <http://www.gnu.org/licenses/>.
+package org.alephium.explorer
+
+import org.scalacheck.Gen
+
+import org.alephium.explorer.persistence.model._
+
+/** Test-data generators for types in package [[org.alephium.explorer.persistence.model]]  */
+object GenModel extends Generators {
+
+  /** Generates and [[org.alephium.explorer.persistence.model.InputEntity]] for the given
+    * [[org.alephium.explorer.persistence.model.OutputEntity]] generator */
+  @SuppressWarnings(Array("org.wartremover.warts.DefaultArguments"))
+  def genInputOutput(
+      outputGen: Gen[OutputEntity] = outputEntityGen): Gen[(InputEntity, OutputEntity)] =
+    for {
+      output <- outputGen
+      input  <- inputEntityGen(output)
+    } yield (input, output)
+
+  /** Convert input-output to [[org.alephium.explorer.persistence.model.TransactionPerAddressEntity]] */
+  def toTransactionPerAddressEntity(input: InputEntity,
+                                    output: OutputEntity): TransactionPerAddressEntity =
+    TransactionPerAddressEntity(
+      hash      = output.txHash,
+      address   = output.address,
+      blockHash = output.blockHash,
+      timestamp = output.timestamp,
+      txOrder   = input.txOrder,
+      mainChain = output.mainChain
+    )
+
+  /** Convert multiple input-outputs to [[org.alephium.explorer.persistence.model.TransactionPerAddressEntity]] */
+  def toTransactionPerAddressEntities(
+      inputOutputs: Iterable[(InputEntity, OutputEntity)]): Iterable[TransactionPerAddressEntity] =
+    inputOutputs map {
+      case (input, output) =>
+        toTransactionPerAddressEntity(input, output)
+    }
+
+}
