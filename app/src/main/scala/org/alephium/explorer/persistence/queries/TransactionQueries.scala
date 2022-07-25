265156d511536b655ac6ad2ce51dec7cb075b368
simerplaha <simer.j@gmail.com>
2022-07-25 19:43:18 +1000

    fix merge conflict

--- app/src/main/scala/org/alephium/explorer/persistence/queries/TransactionQueries.scala
+++ app/src/main/scala/org/alephium/explorer/persistence/queries/TransactionQueries.scala
@@ -31,6 +31,7 @@ import org.alephium.explorer.persistence.queries.InputQueries._
 import org.alephium.explorer.persistence.queries.OutputQueries._
 import org.alephium.explorer.persistence.queries.result._
 import org.alephium.explorer.persistence.schema._
+import org.alephium.explorer.persistence.schema.CustomGetResult._
 import org.alephium.explorer.persistence.schema.CustomJdbcTypes._
 import org.alephium.explorer.persistence.schema.CustomSetParameter._
 import org.alephium.explorer.util.SlickUtil._
