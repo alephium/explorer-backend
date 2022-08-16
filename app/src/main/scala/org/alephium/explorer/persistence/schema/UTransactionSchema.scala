4bb1d9b8c8fdebe1e7917afe5f11edccb46b5ec5
Thomas Droxler <thomas.droxler@gmail.com>
2022-08-16 10:34:51 +0200

    Add index on `last_seen`

--- app/src/main/scala/org/alephium/explorer/persistence/schema/UTransactionSchema.scala
+++ app/src/main/scala/org/alephium/explorer/persistence/schema/UTransactionSchema.scala
@@ -17,7 +17,7 @@
 package org.alephium.explorer.persistence.schema
 
 import slick.jdbc.PostgresProfile.api._
-import slick.lifted.ProvenShape
+import slick.lifted.{Index, ProvenShape}
 
 import org.alephium.explorer.api.model.{GroupIndex, Transaction}
 import org.alephium.explorer.persistence.model.UnconfirmedTxEntity
@@ -35,6 +35,8 @@ object UnconfirmedTxSchema extends Schema[UnconfirmedTxEntity]("utransactions") 
     def gasPrice: Rep[U256] =
       column[U256]("gas_price", O.SqlType("DECIMAL(80,0)")) //U256.MaxValue has 78 digits
     def lastSeen: Rep[TimeStamp] = column[TimeStamp]("last_seen")
+
+    def lastSeenIdx: Index = index("utransactions_last_seen_idx", lastSeen)
 
     def * : ProvenShape[UnconfirmedTxEntity] =
       (hash, chainFrom, chainTo, gasAmount, gasPrice, lastSeen)
