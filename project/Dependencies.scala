aa791954142ffd47427cb45781d404df6a289e26
Thomas Droxler <thomas.droxler@gmail.com>
2025-05-05 14:33:05 +0200

    Update common to `3.14.0`

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@
 import sbt._
 
 object Version {
-  lazy val common = "3.12.7+299-42d5f16b-SNAPSHOT"
+  lazy val common = "3.14.0"
 
   lazy val akka       = "2.6.20"
   lazy val rxJava     = "3.1.8"
