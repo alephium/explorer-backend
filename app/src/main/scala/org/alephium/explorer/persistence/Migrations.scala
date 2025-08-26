ad7a231f89d138f5a7b4b9e5f2a18d74217027fb
Thomas Droxler <thomas.droxler@gmail.com>
2025-08-26 10:10:39 +0200

    Migrate conflicted txs from Danube upgrade

--- app/src/main/scala/org/alephium/explorer/persistence/Migrations.scala
+++ app/src/main/scala/org/alephium/explorer/persistence/Migrations.scala
@@ -18,6 +18,7 @@ import org.alephium.explorer.persistence.model.AppState.MigrationVersion
 import org.alephium.explorer.persistence.queries.AppStateQueries
 import org.alephium.explorer.persistence.schema.CustomGetResult._
 import org.alephium.explorer.persistence.schema.CustomSetParameter._
+import org.alephium.explorer.service._
 import org.alephium.explorer.util.SlickUtil._
 import org.alephium.protocol.model.{Address, BlockHash}
 import org.alephium.protocol.vm.LockupScript
@@ -342,6 +343,7 @@ object Migrations extends StrictLogging {
       versionOpt: Option[MigrationVersion]
   )(implicit
       ec: ExecutionContext,
+      explorerConfig: ExplorerConfig,
       databaseConfig: DatabaseConfig[PostgresProfile]
   ): Future[Unit] = {
     (versionOpt match {
@@ -351,20 +353,31 @@ object Migrations extends StrictLogging {
         Future.unit
       case Some(MigrationVersion(current)) if current > latestVersion.version =>
         throw new Exception("Incompatible migration versions, please reset your database")
-      case Some(MigrationVersion(current)) =>
-        if (current <= 5) {
-          logger.info(s"Background migrations needed, but will be done in a future release")
-          /*
-           * The coinbase migration is heavy and we had some performance issues due to the increase of users.
-           * First, we need to optimize some queries in the syncing process, and then we can re-enable this migration.
-           * For now, we will just update the version and perform the migration in the next release.
-           * We can't just remove it and revert to the previous version because some users might have already completed the migration.
-           */
-          // backgroundCoinbaseMigration()
-          Future.unit
-        } else {
-          Future.unit
-        }
+      case Some(MigrationVersion(_)) =>
+        /*
+         * The coinbase migration is heavy and we had some performance issues due to the increase of users.
+         * First, we need to optimize some queries in the syncing process, and then we can re-enable this migration.
+         * For now, we will just update the version and perform the migration in the next release.
+         * We can't just remove it and revert to the previous version because some users might have already completed the migration.
+         */
+        // logger.info(s"Background migrations needed, but will be done in a future release")
+        // backgroundCoinbaseMigration()
+
+        logger.info("Updating conflicted transactions")
+        implicit lazy val blockFlowClient: BlockFlowClient =
+          BlockFlowClient(
+            uri = explorerConfig.blockFlowUri,
+            groupNum = explorerConfig.groupNum,
+            maybeApiKey = explorerConfig.maybeBlockFlowApiKey,
+            directCliqueAccess = explorerConfig.directCliqueAccess,
+            consensus = explorerConfig.consensus
+          )
+        ConflictedTxsService
+          .updateConflictedTxs(explorerConfig.consensus.danube.forkTimestamp)
+          .flatMap { _ =>
+            logger.info("Conflicted transactions migration done")
+            Future.unit
+          }
     }).flatMap(_ => DBRunner.run(updateVersion(Some(latestVersion))))
   }
 
